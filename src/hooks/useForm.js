0605885a54e45affff854bc115f2da81fe5f6ad0
Cody Singletary <codysingletary92@gmail.com>
2021-04-18 23:21:53 -0400

    plantlist and checkoutform are done, now for the tests.,

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,1 +1,29 @@
-// write your custom hook here to control your checkout form
+import {useState} from 'react';
+export const useForm = () => {
+    const initialValues = {
+        firstName: "",
+        lastName: "",
+        address: "",
+        city: "",
+        state: "",
+        zip: "",
+    };
+
+    const [values, setValues] = useState(initialValues);
+
+    const handleChanges = e => {
+        setValues({
+            ...values,
+            [e.target.name]: e.target.value
+        });
+    };
+
+const clearForm = e => {
+    e.preventDefault();
+    setValues(initialValues)
+}
+
+    return [values, handleChanges, clearForm];
+};
+
+export default useForm;
